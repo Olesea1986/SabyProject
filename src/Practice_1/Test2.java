6e1649db70ad76b84dd0f5a1ffc4182cdf44774f
Olesea1986 <olesea86williams@gmail.com>
2021-01-26 07:33:29 -0600

    second commit

--- src/Practice_1/Test2.java
+++ src/Practice_1/Test2.java
@@ -3,5 +3,6 @@ package Practice_1;
 public class Test2 {
     public static void main(String[] args) {
         // created my second class
+
     }
 }
---
+++ src/Practice_1/Test3.java
@@ -0,0 +1,6 @@
+package Practice_1;
+
+public class Test3 {
+    // some new code
+    // extra new features
+}
